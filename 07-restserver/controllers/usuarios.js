9e6eb250308f677d36ed55deeb8943979dd71bd1
AzureAD\ManuelGonzalezPerez <mgonpere@everis.com>
2022-06-21 15:02:27 +0200

    cafe

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -1,4 +1,7 @@
 const { response } = require('express');
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
+
 
 const usuariosGet = (req, res) =>{
 
@@ -13,14 +16,24 @@ const usuariosGet = (req, res) =>{
 }
 
 
-const usuariosPost = (req, res = response) =>{
-    console.log(req.body)
-    const body = req.body;
+const usuariosPost = async (req, res = response) =>{
+   
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo, password, rol});
 
+    //Verificar si el correo existe
+
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync(); 
+    usuario.password = bcryptjs.hashSync(password, salt)
+
+
+    //Guardar en DB
+    await usuario.save();
 
     res.json({
         msg: 'post API-usuariosPost',
-        body
+        usuario
     })
 }
 
---
+++ 07-restserver/models/usuario.js
@@ -0,0 +1,36 @@
+
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo:{
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    imagen:{
+        type: String
+    },
+    rol:{
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    estado: {
+        type: Boolean,
+        default:true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = model('Usuarios', UsuarioSchema)
